c7b349ed15c6bef1f1ea6b95a97c49409eb10062
vicash <vicash@tibura.com>
2025-03-03 12:50:10 +0530

    trascation table updated

--- src/main/java/com/vi/base/modules/Pxt_fac_hdrs/Pxt_fac_hdrRepository.java
+++ src/main/java/com/vi/base/modules/Pxt_fac_hdrs/Pxt_fac_hdrRepository.java
@@ -2,14 +2,14 @@
  - Version Number 0.0.1
 */
 
-package com.sm.cars.base.modules.allotments;
+package com.vi.base.modules.Pxt_fac_hdrs;
 
-import com.vi.model.dao.AllotmentDAO;
+import com.vi.model.dao.Pxt_fac_hdrDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AllotmentRepository extends JpaRepository<AllotmentDAO,Long>, JpaSpecificationExecutor<AllotmentDAO> {
+public interface Pxt_fac_hdrRepository extends JpaRepository<Pxt_fac_hdrDAO,Long>, JpaSpecificationExecutor<Pxt_fac_hdrDAO> {
 
 }
--- src/main/java/com/vi/corelib/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/vi/corelib/exceptions/ApiExceptionHandler.java
@@ -22,72 +22,56 @@ public class ApiExceptionHandler {
     @ResponseBody
     @ExceptionHandler(EntityNotFoundException.class)
     public  ResponseError handleValidationError(EntityNotFoundException ex) {
-        //BindingResult bindingResult = ex.getBindingResult();
-        //FieldError fieldError = bindingResult.getFieldError();
-        //String defaultMessage = fieldError.getDefaultMessage();
+
         return new ResponseError("-2000", "DATA_NOT_FOUND",ex.getMessage());
     }
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ResponseBody
     @ExceptionHandler(NullPointerException.class)
     public  ResponseError handleValidationError(NullPointerException ex) {
-        //BindingResult bindingResult = ex.getBindingResult();
-        //FieldError fieldError = bindingResult.getFieldError();
-        //String defaultMessage = fieldError.getDefaultMessage();
+        
         return new ResponseError("-2001", "MISSING_VALUES",ex.getMessage());
     }
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ResponseBody
     @ExceptionHandler(DataIntegrityViolationException.class)
     public  ResponseError handleValidationError(DataIntegrityViolationException ex) {
-        //BindingResult bindingResult = ex.getBindingResult();
-        //FieldError fieldError = bindingResult.getFieldError();
-        //String defaultMessage = fieldError.getDefaultMessage();
+        
         return new ResponseError("-2002", "INTEGRITY_VIOLOATION",ex.getMessage());
     }
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ResponseBody
     @ExceptionHandler(AuthenticationException.class)
     public  ResponseError handleValidationError(AuthenticationException ex) {
-        //BindingResult bindingResult = ex.getBindingResult();
-        //FieldError fieldError = bindingResult.getFieldError();
-        //String defaultMessage = fieldError.getDefaultMessage();
+        
         return new ResponseError("-4000", "ACCESS DENIED/INVALID OR MISSING TOKEN",ex.getMessage());
     }
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ResponseBody
     @ExceptionHandler(TokenException.class)
     public  ResponseError handleValidationError(TokenException ex) {
-        //BindingResult bindingResult = ex.getBindingResult();
-        //FieldError fieldError = bindingResult.getFieldError();
-        //String defaultMessage = fieldError.getDefaultMessage();
+        
         return new ResponseError("-4001", "TOKEN_NOT_FOUND",ex.getMessage());
     }
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ResponseBody
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public  ResponseError handleValidationError(HttpMessageNotReadableException ex) {
-        //BindingResult bindingResult = ex.getBindingResult();
-        //FieldError fieldError = bindingResult.getFieldError();
-        //String defaultMessage = fieldError.getDefaultMessage();
+        
         return new ResponseError("-4001", "Missing Body/Parameters/Path",ex.getMessage());
     }
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ResponseBody
     @ExceptionHandler(MethodNotAllowedException.class)
     public  ResponseError handleValidationError(MethodNotAllowedException ex) {
-        //BindingResult bindingResult = ex.getBindingResult();
-        //FieldError fieldError = bindingResult.getFieldError();
-        //String defaultMessage = fieldError.getDefaultMessage();
+        
         return new ResponseError("-4001", "Missing Body/Parameters/Path",ex.getMessage());
     }
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public  ResponseError handleValidationError(Exception ex) {
-        //BindingResult bindingResult = ex.getBindingResult();
-        //FieldError fieldError = bindingResult.getFieldError();
-        //String defaultMessage = fieldError.getDefaultMessage();
+         System.out.print(Exception.class);
         return new ResponseError("-4001", "Missing Body/Parameters/Path",ex.getMessage());
     }
 }
